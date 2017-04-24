e50df21aa442beb9735ac4efbb4782e9c662473b
justttry <kerzhao@163.com>
2017-04-24 17:32:29 +0800

    update

--- captcha_break_v2_3_test3.py
+++ captcha_break_v2_3_test3.py
@@ -2,19 +2,33 @@ import cv2
 import os
 import numpy as np
 import random
-from generator import captcha_generator as gen
+#from generator import captcha_generator as gen
+from captcha.image import ImageCaptcha
         
 from keras.models import *
 from keras.layers import *
 
-chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabdefghijlmnqrtuwxy"
+chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 width, height, n_len, n_class = 250, 80, 6, len(chars)
+
+def gen(batch_size=32):
+    X = np.zeros((batch_size, height, width, 3), dtype=np.uint8)
+    y = [np.zeros((batch_size, n_class), dtype=np.uint8) for i in range(n_len)]
+    generator = ImageCaptcha(width=width, height=height)
+    while True:
+        for i in range(batch_size):
+            random_str = ''.join([random.choice(chars) for j in range(6)])
+            X[i] = generator.generate_image(random_str)
+            for j, ch in enumerate(random_str):
+                y[j][i, :] = 0
+                y[j][i, chars.find(ch)] = 1
+        yield X, y
 
 input_tensor = Input((height, width, 3))
 x = input_tensor
 for i in range(4):
-    x = Convolution2D(32*2**i, (3, 3), activation='relu')(x)
-    x = Convolution2D(32*2**i, (3, 3), activation='relu')(x)
+    x = Conv2D(32*2**i, (3, 3), activation='relu')(x)
+    x = Conv2D(32*2**i, (3, 3), activation='relu')(x)
     x = MaxPooling2D((2, 2))(x)
 
 x = Flatten()(x)
@@ -27,8 +41,8 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 
-model.fit_generator(gen(width=width, height=height), steps_per_epoch=2000, epochs=20, 
-                    validation_data=gen(width=width, height=height), validation_steps=500)
+model.fit_generator(gen(), steps_per_epoch=2000, epochs=20, 
+                    validation_data=gen(), validation_steps=500)
 
 model.save('mycnn_v20170424_adagrad.h5')
 print 'saved mycnn_v20170424_adagrad.h5'
@@ -36,7 +50,7 @@ print 'saved mycnn_v20170424_adagrad.h5'
 from tqdm import tqdm
 def evaluate(model, batch_num=20):
     batch_acc = 0
-    generator = gen(width=width, height=height)
+    generator = gen(1)
     for i in tqdm(range(batch_num)):
         X, y = generator.next()
         y_pred = model.predict(X)
