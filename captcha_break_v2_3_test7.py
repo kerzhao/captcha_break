9e6f5c17613eccd488fe552b6c3999f2804060c5
justttry <kerzhao@163.com>
2017-04-28 15:32:32 +0800

    update

--- captcha_break_v2_3_test7.py
+++ captcha_break_v2_3_test7.py
@@ -32,8 +32,8 @@ model.compile(loss='categorical_crossentropy',
 model.fit_generator(gen(width=width, height=height), steps_per_epoch=2000, epochs=10, 
                     validation_data=gen(width=width, height=height), validation_steps=500)
 
-model.save('mycnn_v201704271006_v6_adadelta.h5')
-print 'saved mycnn_v201704271006_v6_adadelta.h5'
+model.save('mycnn_v201704271006_v7_adadelta.h5')
+print 'saved mycnn_v201704271006_v7_adadelta.h5'
 
 from tqdm import tqdm
 def evaluate(model, batch_num=20):
